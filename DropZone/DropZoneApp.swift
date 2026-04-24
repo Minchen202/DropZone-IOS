d6bc5a3b1ea5bef9cf05b49ef3d66a9b897572cd
Jannik <jannik.hinkelmann@gmx.de>
2026-04-24 12:39:37 +0200

    Initial Commit

---
+++ DropZone/DropZoneApp.swift
@@ -0,0 +1,32 @@
+//
+//  DropZoneApp.swift
+//  DropZone
+//
+//  Created by Gastbenutzer:in on 24.04.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DropZoneApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DropZone/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DropZone
+//
+//  Created by Gastbenutzer:in on 24.04.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
